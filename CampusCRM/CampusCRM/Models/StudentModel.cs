8274f9cda20f818f56d2da98dbd37d409c97094b
DmitriyZalesin <zalesindmitriy@gmail.com>
2021-08-04 15:11:06 +0300

    change validation attr

--- CampusCRM/CampusCRM/Models/StudentModel.cs
+++ CampusCRM/CampusCRM/Models/StudentModel.cs
@@ -8,9 +8,23 @@ namespace CampusCRM.Models
 {
     public class StudentModel
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "This field can be contain only letters")]
+        [Display(Name = "Name", Prompt = "Name")]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "This field can be contain only letters")]
+        [Display(Name = "Surname", Prompt = "Surname")]
         public string Surname { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?:100|1[5-9]|[2-9][0-9])$", ErrorMessage = "Your age should be 15-100")]
         public int Age { get; set; }
     }
 }
