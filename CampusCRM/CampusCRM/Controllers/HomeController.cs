3a151ca71444eda59f306e8c90093d3f2acd2a12
Dmitriy Zalesin <dmnkyou@gmail.com>
2021-09-08 21:18:10 +0300

    add Role Manager of all users

--- CampusCRM/CampusCRM/Controllers/HomeController.cs
+++ CampusCRM/CampusCRM/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace CampusCRM.MVC.Controllers
 {
-   // [AllowAnonymous]
+   [AllowAnonymous]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
---
+++ CampusCRM/CampusCRM/Models/RoleManagerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace CampusCRM.MVC.Models
+{
+    public class RoleManagerModel
+    {
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+        public List<IdentityRole> AllRoles { get; set; }
+        public IList<string> UserRoles { get; set; }
+
+        public RoleManagerModel()
+        {
+            AllRoles = new List<IdentityRole>();
+            UserRoles = new List<string>();
+        }
+    }
+}
