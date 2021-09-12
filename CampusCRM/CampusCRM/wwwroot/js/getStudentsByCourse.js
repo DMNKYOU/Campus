e819255c48e21e73eac67cb671a90a03af7792dc
Dmitriy Zalesin <dmnkyou@gmail.com>
2021-09-12 18:07:23 +0300

    fix js

--- CampusCRM/CampusCRM/wwwroot/js/getStudentsByCourse.js
+++ CampusCRM/CampusCRM/wwwroot/js/getStudentsByCourse.js
@@ -39,14 +39,11 @@ function showStudents(students) {
 
     $("#students").append(`<label class="col-sm-2 col-form-label">Students</label>`);
 
-    $("#students").append(`
-        <ul class="list-students">
-        </ul>
+    $("#students").append(`<ul class="col-sm-10"></ul>
     `);
 
     students.forEach((student) => {
-        
-            $(".list-students").append(`<li class="list-group-item">
+        $(".list-students").append(`<li class="list-group-item">
                     <label class="list-item">${student.name} ${student.surname}
                         <input type="checkbox" name="studentsId" value="${student.id}" id="checkboxtoedit${student.id}" >
                     </label>
